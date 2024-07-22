76eba721160ce0d493aef2abfce101068ec4af42
masterMav <mavdoesdev@gmail.com>
2024-07-22 12:15:09 +0530

    added indexer tests

--- helpers/psqlHelper.js
+++ helpers/psqlHelper.js
@@ -442,6 +442,63 @@ const saveIndexerConstants = async () => {
     }
 };
 
+const _testForDuplicateLogs = async (chainId, txnHash, logIndex) => {
+    try {
+        const tableDetails = psql.tables.logs(chainId);
+
+        const data = await client.query(`SELECT COUNT(*) FROM ${tableDetails.name} 
+                                         WHERE ${tableDetails.columns.txnHash.field}='${txnHash}' AND
+                                               ${tableDetails.columns.logIndex.field}='${logIndex}';
+                                       `);
+
+        return data.rows[0].count == '1';
+    } catch (err) {
+        console.error(`Error testing for duplicate logs.`, err.message);
+    }
+};
+
+const _testForDuplicateTxns = async (chainId, txnHash) => {
+    try {
+        const tableDetails = psql.tables.txn(chainId);
+
+        const data = 
+            await client.query(`SELECT COUNT(*) FROM ${tableDetails.name} 
+                                WHERE ${tableDetails.columns.txnHash.field}='${txnHash}';
+                               `);
+
+        return data.rows[0].count == '1';
+    } catch (err) {
+        console.error(`Error testing for duplicate txns.`, err.message);
+    }
+};
+
+const _getAllTableNames = async () => {
+    try {
+        const data = 
+            await client.query(`SELECT table_name FROM information_schema.tables
+                                WHERE table_schema = 'public';
+                               `);
+
+        return data.rows || [];
+    } catch (err) {
+        console.error(`Error getting all tablenames.`, err.message);
+    }
+}
+
+const _getTxnByHash = async (chainId, txnHash) => {
+    try {
+        const tableDetails = psql.tables.txn(chainId);
+
+        const data = 
+            await client.query(`SELECT * FROM ${tableDetails.name} WHERE ${tableDetails.columns.txnHash.field}='${txnHash}';
+                               `);
+
+        return data.rows || [];
+    } catch (err) {
+        console.error(`Error getting txn by hash.`, err.message);
+    }
+}
+
 module.exports = {
     init,
     insert,
@@ -450,4 +507,8 @@ module.exports = {
     _getLastTrackedBlocksDb,
     getTxnsCountFromDb,
     getAllBlockNumbers,
+    _testForDuplicateLogs,
+    _testForDuplicateTxns,
+    _getAllTableNames,
+    _getTxnByHash
 };
